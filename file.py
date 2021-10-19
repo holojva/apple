a4ad1dfd063c961376938c5c30743a1543f62cfa
holojva <holojva@gmail.com>
2021-10-19 19:58:02 +0300

    ya vse sdelal

--- file.py
+++ file.py
@@ -2,23 +2,34 @@ from typing import cast
 import requests
 from bs4 import BeautifulSoup
 url = f"https://kidkodschool.github.io/welcome"
-
+import sys
+import random
 response = requests.get(url)
-
-print(dir(response))
-print(response.status_code)
-print(response.headers)
+query = sys.argv[1] if len(sys.argv) > 1 else input("ima avatara ")
+# print(dir(response))
+# print(response.status_code)
+# print(response.headers)
 # with open("./python_is_cool.html", "wb") as f :
 #     f.write(response.content)
-query = "cats"
-url = f"https://kiddle.co/s.php?q={query}"
+# query = "cats"
+url = f"https://google.com/images?q={query}"
 page = requests.get(url).text
 soup = BeautifulSoup(page, "html.parser")
-print(soup)
+count = 0
+randnum = random.randint(1, 20)
 for raw_img in soup.find_all("img"):
+    print("Searching...")
     link = raw_img.get("src")
-    if link and link.startswith("https") :
-        response = requests.get(link)
-        with open("./today_avatar.jpg", "wb") as f :
-            f.write(response.content)
-        break
+    alternative = raw_img.get("alt")
+    if link and link.startswith("https") and alternative != "Google":
+        count += 1
+        if randnum == count :
+            response = requests.get(link)
+            with open(f"./today_avatar.jpg", "wb") as f :
+                f.write(response.content)
+                print("Found!")
+                break
+    # elif link and link.startswith("/images/") :
+    #     response = requests.get("https://www.google.com" + link)
+    #     with open(f"./today_avatar{num}.jpg", "wb") as f :
+    #         f.write(response.content)
